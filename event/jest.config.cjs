581c850025d83ccf302bf9fd86119b0b6b84f9de
xemayebenes <xema@devgurus.io>
2023-11-27 12:16:23 +0100

    shared

--- event/jest.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  displayName: 'Tests Javascript Application - Event',
-  moduleDirectories: ['node_modules', 'src'],
-  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  testEnvironment: 'node',
-};
